38c79f130e19f624234556eb00f83e9421086a33
Mateo Hernandez <mateo.hernandez_idrovo@tufts.edu>
2021-05-02 16:19:43 -0500

    OK Chris this should work. Success would mean that you are redirected to a page after logging in. Evan: we need to find a way to redirect them to homepage or something. Additionally, it is important to note that we are still to do API call, which I can work on at night with Evan. We need the pages ready by then. Thank you

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,16 @@ const router = require('express').Router();
 const User = require('../User');
 const Joi = require('joi');
 const jwt = require('jsonwebtoken');
+var fs = require('fs');
 var querystring = require('querystring');
+const express_session = require('express-session');
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb+srv://mhidrovo:aaa@cluster0.jwixh.mongodb.net/Drinks?\
              retryWrites=true&w=majority";
 
 
+
+var isLoggedIn = false;
 
 
 
@@ -31,39 +35,42 @@ const schema = Joi.object({
 */
 router.post('/register', async (req, res) =>
 {
+    MongoClient.connect(url, { useUnifiedTopology: true }, async function(err, db) {
+        if(err) { 
+            console.log("Connection err: " + err); return; 
+        }
+        var dbo = db.db("Drinks");
+        var coll = dbo.collection('users');
+        console.log("before find");
+        console.log("Username is: " + input_data['username'] + "    Password: " + input_data['password']);
+        user_query = {username:input_data['username']};
+        coll.find(user_query).toArray(async function(err, items) {
+            if (err) {
+                console.log("Error: " + err);
+            } 
+            else 
+            {
+                // if(input_data['password'] != items[0].password)
+                //     return res.send("Incorrect password");
+                if (items.length == 0)
+                {
+                    coll.insertOne({"username":input_data['username'], "password":input_data['password']})
+                    /* We should return to homepage  */
+                    .then(res.redirect('https://medium.com/swlh/fetch-requests-and-controller-actions-connecting-the-frontend-to-the-backend-733a87ffe757'));
 
-    
-    //ensures proper username/password for registering
-    const { error } = schema.validate({username: req.body.username, password: req.body.password});
-    if (error) {
-        return res.status(400).send(error.details[0].message);
-    }
-    
-    //checks to see if username already exists in database
-    const usernameExists = await User.findOne({username: req.body.username});
-    if (usernameExists) return res.status(400).send("Username already exists.");
-    
-    //creates a new user for the database
-    const user = new User({
-        username: req.body.username,
-        password: req.body.password
+                }
+            }
+        });
     });
-    
-    //will send if valid username, else will produce an error
-    try{
-        const savedUser = await user.save();
-        res.send(savedUser);
-    }catch(err){
-        res.status(400).send(err);
-    }
-
 });
 
-
 router.post('/login', async (req, res) => {
-     //checks to see if username already exists in database
-    //  res.writeHead(200, {'Content-Type': 'text/html'});
-    // res.write("<h2> PROCESSING FORM </h2>");
+    console.log("here!");
+    // file = 'login.html';
+    // fs.readFile(file, function(err, txt){
+    //     res.writeHead(200, {'Content-Type': 'text/html'});
+    //     res.write(txt);
+    // })
      input_data = "";
      req.on('data', data => {
         input_data += data.toString();
@@ -81,6 +88,7 @@ router.post('/login', async (req, res) => {
         var dbo = db.db("Drinks");
         var coll = dbo.collection('users');
         console.log("before find");
+        console.log("Username is: " + input_data['username'] + "    Password: " + input_data['password']);
         user_query = {username:input_data['username']};
         coll.find(user_query).toArray(function(err, items) {
           if (err) {
@@ -94,61 +102,12 @@ router.post('/login', async (req, res) => {
                 return res.send("Not found");
             if(input_data['password'] != items[0].password)
                 return res.send("Incorrect password");
-            const token = jwt.sign({_id: items[0]._id}, 'logged_in_for_now'); 
-            res.header('auth-token', token).send(token);
-            // localStorage.setItem('auth-token', token);
+                isLoggedIn = true;
+                return res.redirect('https://medium.com/swlh/fetch-requests-and-controller-actions-connecting-the-frontend-to-the-backend-733a87ffe757');
           }   
           db.close();
         });
     });
-
-
-
-
-
-     
-
-    // const user = await User.find({username: input_data['username']});
-    // var stream = await User.find({username:input_data['username']});
-    // stream.on('data', function(users){
-    //     users.forEach(function(user){
-    //        console.log(user.username);
-    //     });
-    // });
-    // stream.on('error', function(error){
-    //     console.log(error);
-    // });
-
-
-    // const Dan = User.find({username:"Dan"});
-    // console.log(Dan.username);
-    // User.find({ username: input_data['username'] }).then(users => {              
-    //     console.log(users[0].username); // 'A'
-    // });
-
-
-
-
-
-
-    // console.log("Our username is: " + user[0].username);
-    // console.log("Username is " + user.username + "and pasword:" + user.password);
-
-
-    // if (user == null) {
-    //     console.log(input_data['username'] + " is the username");
-    //     return res.status(400).send("This means username doesn't exist");
-    // } 
-
-    // // console.log("Password stored in DB is: " + user[0].password);
-    // console.log("Password inputted by user is: " + input_data['password']);
-   
-    // // if ( input_data['password'] != user.password ) return res.status(400).send("The passwords do not match");
-
-    // Creating and assign Web Token 
-
-    // res.send();
-    
 })
 
 
