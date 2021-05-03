cd0a5a77ca73bab5fac51ea25f5679e66e948734
Mateo Hernandez <mateo.hernandez_idrovo@tufts.edu>
2021-05-02 23:00:56 -0500

    added documentation related to apirequest.js. Feel free to use it in our code

--- frontend/apirequest.js
+++ frontend/apirequest.js
@@ -3,7 +3,35 @@
 const obj_query = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 
 
-/* What fetch info does is  */
+/* What apiFetch does is it gets the ingredients from user input
+ * It later forms the query string to pass to the helper function
+ * fetchInfo. 
+ * It operates in a loop through the elements array of ingredients. 
+ * 
+ */
+function apiFetch()
+        {
+            var query_to_send = "";
+            arr_ingredients = document.getElementsByName("ingredient1");
+            for(var i = 0; i < arr_ingredients.length; i++)
+            {
+                if (arr_ingredients[i] != "")
+                    query_to_send += "i=" + arr_ingredients[i].value
+                if(i != arr_ingredients.length - 1)
+                    query_to_send += "&";    
+            }
+
+            var full_query = http_path + query_to_send;
+            document.getElementById('fetch_request').value = full_query;
+            console.log("hello")
+            fetchInfo(full_query)
+        }
+
+/* Function fetchInfo 
+ * This fetches BY INGREDIENTS. 
+ * NOTE: Since displaying by name, it immediately calls on helper function
+ * displayJSONObject to look up the name's drink and then display its information.
+ */
 function fetchInfo(full_query)
 {
     fetch(full_query)
@@ -26,7 +54,14 @@ function fetchInfo(full_query)
     });
 
 }
-
+/* 
+ * Does another API fetch request based on name to retrieve objects with more
+ * information. 
+ * Currently, it creates images based on the image source returned in each of
+ * the drinks array. 
+ * Run this locally with cocktail.html to see the displayed result in the 
+ * console. 
+ */
 
 function displayJSONObject(object_name)
 {
